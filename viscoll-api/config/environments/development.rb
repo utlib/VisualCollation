da8fdb84256d560c7813ff2392b3e9b64cea9fb8
Doug Emery <emeryr@upenn.edu>
2020-08-17 17:00:05 -0400

    WIP: Troubleshoot dev SMTP issues

--- viscoll-api/config/environments/development.rb
+++ viscoll-api/config/environments/development.rb
@@ -31,13 +31,13 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.default_url_options = { :host => "localhost", :port => 3000 }
-  # config.action_mailer.smtp_settings = {
-  #   :address => 'smtp.ethereal.email',
-  #   :port => 587,
-  #   :user_name => 'korey33@ethereal.email',
-  #   :password => 'NJZhrZYbDCeZu2JUB3'
-  # }
+  config.action_mailer.default_url_options = { :host => "localhost", :port => 3000 }
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.ethereal.email',
+    :port => 587,
+    :user_name => 'brady.wiegand@ethereal.email',
+    :password => '2DzDyRf6Q22n21FV2q'
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
