c2fe0aa524510243cb05f5f65449c9deca5078b2
lilblizzard <lilblizzard97@gmail.com>
2020-08-18 13:58:51 -0400

    updated development environment to see changes to backend in real time

--- viscoll-api/config/environments/development.rb
+++ viscoll-api/config/environments/development.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.file_watcher = ActiveSupport::FileUpdateChecker
 
   config.middleware.insert_before 0, Rack::Cors do
     allow do
